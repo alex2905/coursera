96498af8b59521586f5d565ba4bc98aea461cd17
Alex Kurtz <alex.kurtz@web.de>
2018-01-05 19:56:43 +0100

    uncomment all examples

--- R/fars_functions.R
+++ R/fars_functions.R
@@ -23,10 +23,10 @@
 #' @return A tibble dataframe containing the information of the csv file
 #' @export
 #' @examples
-#' filename <- "accident_2013.csv.bz2"
-#' accident_2013 <- fars_read(filename)
-#' filename <- "accident_2015.csv.bz2"
-#' accident_2013 <- fars_read(filename)
+#' # filename <- "accident_2013.csv.bz2"
+#' # accident_2013 <- fars_read(filename)
+#' # filename <- "accident_2015.csv.bz2"
+#' # accident_2013 <- fars_read(filename)
 fars_read <- function(filename) {
   if(!file.exists(filename))
     stop("file '", filename, "' does not exist")
@@ -52,8 +52,8 @@ fars_read <- function(filename) {
 #'         accident_year.csv.bz2
 #' @export
 #' @examples
-#' zipped_file <- make_filename(2013)
-#' make_filename(2014)
+#' # zipped_file <- make_filename(2013)
+#' # make_filename(2014)
 make_filename <- function(year) {
   year <- as.integer(year)
   sprintf("accident_%d.csv.bz2", year)
@@ -85,8 +85,8 @@ make_filename <- function(year) {
 #'         the file name.
 #' @export
 #' @examples
-#' fars_read_years(list(2013,2014,2015))
-#' fars_read_years(2013)
+#' # fars_read_years(list(2013,2014,2015))
+#' # fars_read_years(2013)
 fars_read_years <- function(years) {
   lapply(years, function(year) {
     file <- make_filename(year)
@@ -125,8 +125,8 @@ fars_read_years <- function(years) {
 #'         observations for each month in each of the input years
 #' @export
 #' @examples
-#' fars_summarize_years(list(2013,2014,2015))
-#' fars_summarize_years(2013)
+#' # fars_summarize_years(list(2013,2014,2015))
+#' # fars_summarize_years(2013)
 fars_summarize_years <- function(years) {
   dat_list <- fars_read_years(years)
   dplyr::bind_rows(dat_list) %>%
@@ -163,8 +163,8 @@ fars_summarize_years <- function(years) {
 #'         the chosen state and year
 #' @export
 #' @examples
-#' fars_map_state(state.num = 1, year = 2014)
-#' fars_map_state(state.num = 9, year = 2013)
+#' # fars_map_state(state.num = 1, year = 2014)
+#' # fars_map_state(state.num = 9, year = 2013)
 fars_map_state <- function(state.num, year) {
   filename <- make_filename(year)
   data <- fars_read(filename)
