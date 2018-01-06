dd84cf1338a05c44d16fdb778256a2cb3cc75114
Alex Kurtz <alex.kurtz@web.de>
2018-01-06 10:43:45 +0100

    examples excluded

--- R/fars_functions.R
+++ R/fars_functions.R
@@ -23,10 +23,10 @@
 #' @return A tibble dataframe containing the information of the csv file
 #' @export
 #' @examples
-#' filename <- "data/accident_2013.csv.bz2"
-#' accident_2013 <- fars_read(filename)
-#' filename <- "data/accident_2015.csv.bz2"
-#' accident_2015 <- fars_read(filename)
+#' # filename <- "data/accident_2013.csv.bz2"
+#' # accident_2013 <- fars_read(filename)
+#' # filename <- "data/accident_2015.csv.bz2"
+#' # accident_2015 <- fars_read(filename)
 fars_read <- function(filename) {
   if(!file.exists(filename))
     stop("file '", filename, "' does not exist")
