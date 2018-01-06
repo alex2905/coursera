4687c49f74ac5dfd5d0009a5d683a4b2b9fcd9e7
Alex Kurtz <alex.kurtz@web.de>
2018-01-06 16:29:36 +0100

    test corrected

--- tests/testthat/test_make_filename.R
+++ tests/testthat/test_make_filename.R
@@ -7,7 +7,10 @@ test_that("make_filename output is correct",{
   # Expectation: Warning message due to input cannot be coerced to integer
   expect_warning(make_filename("hello"))
   # Expectation: Output is correct
-  expect_equal(make_filename(2016), "inst/extdata/accident_2016.csv.bz2")
+  expect_equal(make_filename(2013), system.file("extdata",
+                                                sprintf("accident_%d.csv.bz2",
+                                                        2013),
+                                                package = "coursera"))
 })
 
 test_that("make_filename stops correctly",{
